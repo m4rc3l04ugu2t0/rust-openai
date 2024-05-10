f3d3e6b55870aef2bc52c1464a8b6441545b05df
Marcelo <mrcelo014@gmail.com>
2024-05-09 22:52:48 -0300

    load or create

--- src/ais/assit/mod.rs
+++ src/ais/assit/mod.rs
@@ -1,13 +1,17 @@
-use async_openai::types::{AssistantTools, AssistantToolsRetrieval, CreateAssistantRequest};
+use async_openai::types::{
+	AssistantObject, AssistantTools, AssistantToolsRetrieval, CreateAssistantRequest,
+};
 use derive_more::{Deref, Display, From};
 
 use crate::Result;
 
 use crate::ais::OaClient;
 
+const DEFAULT_QUERY: &[(&str, &str)] = &[("limit", "100")];
+
 pub struct CreateConfig {
-    pub name: String,
-    pub model: String,
+	pub name: String,
+	pub model: String,
 }
 
 #[derive(Debug, From, Deref, Display)]
@@ -20,16 +24,63 @@ pub struct ThreadId(String);
 pub struct FileId(String);
 
 pub async fn create(oac: &OaClient, config: CreateConfig) -> Result<AsstId> {
-    let ao_assts = oac.assistants();
+	let ao_assts = oac.assistants();
 
-    let asst_obj = ao_assts
-        .create(CreateAssistantRequest {
-            model: config.model,
-            name: Some(config.name),
-            tools: Some(vec![AssistantToolsRetrieval::default().into()]),
-            ..Default::default()
-        })
-        .await?;
+	let asst_obj = ao_assts
+		.create(CreateAssistantRequest {
+			model: config.model,
+			name: Some(config.name),
+			tools: Some(vec![AssistantToolsRetrieval::default().into()]),
+			..Default::default()
+		})
+		.await?;
 
-    Ok(asst_obj.id.into())
+	Ok(asst_obj.id.into())
+}
+
+pub async fn load_or_create_asst(
+	oac: &OaClient,
+	config: CreateConfig,
+	recreate: bool,
+) -> Result<AsstId> {
+	let asst_obj = first_by_name(oac, &config.name).await?;
+	let mut asst_id = asst_obj.map(|o| AsstId::from(o.id));
+
+	if let (true, Some(asst_id_ref)) = (recreate, asst_id.as_ref()) {
+		// FIXME: delete(oac, asst_id_ref).await?;
+		asst_id.take();
+		println!("Assistant {} deleted", config.name);
+	}
+
+	if let Some(asst_id) = asst_id {
+		println!("Assistant {} loaded", config.name);
+		Ok(asst_id)
+	} else {
+		let asst_name = config.name.clone();
+		let asst_id = create(oac, config).await?;
+		Ok(asst_id)
+	}
+}
+
+pub async fn first_by_name(
+	oac: &OaClient,
+	name: &str,
+) -> Result<Option<AssistantObject>> {
+	let os_assts = oac.assistants();
+
+	let assts = os_assts.list(DEFAULT_QUERY).await?.data;
+
+	let assts_obj = assts
+		.into_iter()
+		.find(|a| a.name.as_ref().map(|n| n == name).unwrap_or(false));
+
+	Ok(assts_obj)
+}
+
+pub async fn delete(oac: &OaClient, asst_id: &AsstId) -> Result<()> {
+	let oa_assts = oac.assistants();
+
+	oa_assts.delete(&asst_id).await?;
+
+	Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,21 @@ pub use self::error::{Error, Result};
 
 #[tokio::main]
 async fn main() {
-    match start().await {
-        Ok(_) => println!("\nBye!\n"),
-        Err(e) => println!("\nError: {}\n", e),
-    }
+	match start().await {
+		Ok(_) => println!("\nBye!\n"),
+		Err(e) => println!("\nError: {}\n", e),
+	}
 }
 
 async fn start() -> Result<()> {
-    let oac = new_oa_client()?;
+	let oac = new_oa_client()?;
 
-    let asst_config = CreateConfig {
-        name: "buddy-02".to_string(),
-        model: "gpt-3.5-turbo-0125".to_string(),
-    };
-    let asst_id = assit::create(&oac, asst_config).await?;
+	let asst_config = CreateConfig {
+		name: "buddy-02".to_string(),
+		model: "gpt-3.5-turbo-0125".to_string(),
+	};
+	let asst_id = assit::load_or_create_asst(&oac, asst_config, false).await?;
 
-    println!("-->> {asst_id:?}");
-    Ok(())
+	println!("-->> {asst_id:?}");
+	Ok(())
 }
